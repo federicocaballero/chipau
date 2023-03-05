ca031ef0f5135d9fe3ba48f2dd71e9738c9b161b
Federico Caballero <fedecaballero1234@gmail.com>
2023-03-05 12:44:12 -0300

    Bootstrap agregado para pruebas en Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import PizzaCard from './components/pizzaCard';
 
 function App() {
   return (
     <div>
       <h1>holis</h1>
+      <PizzaCard/>
     </div>
   );
 }
---
+++ src/components/pizzaCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PizzaCard = () => {
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default PizzaCard;
